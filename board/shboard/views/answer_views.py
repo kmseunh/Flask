d91e38bc890acc3e52dfb515ce9b340f6c3fa02c
kmseunh <tmdgus8779@gmail.com>
2023-04-28 01:24:57 +0900

    fix: modifying the answer registration routing function

--- board/shboard/views/answer_views.py
+++ board/shboard/views/answer_views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
 from shboard import db
+from shboard.forms import AnswerForm
 from shboard.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
@@ -11,9 +12,12 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=('POST',))
 def create(question_id):
-    question = Question.query.get_or_404(question_id)
-    content = request.form['content']               #--- POST 폼 방식으로 전송된 데이터 항목 중 name 속성이 content인 값
-    answer = Answer(content=content, create_date=datetime.now())
-    question.answer_set.append(answer)              #--- 질문에 달린 답변들
-    db.session.commit()
+    form            = AnswerForm()
+    question        = Question.query.get_or_404(question_id)
+    if form.validate_on_submit():
+        content     = request.form['content']               #--- POST 폼 방식으로 전송된 데이터 항목 중 name 속성이 content인 값
+        answer      = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)                  #--- 질문에 달린 답변들
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
     return redirect(url_for('question/detail', question_id=question_id))
