d871e05870dae3ec2dbcd2968adbbec1dd299cc6
kmseunh <tmdgus8779@gmail.com>
2023-05-17 23:33:22 +0900

    Feature: Creat login routing for user login
    Add a function to handle login
    
    This code contains the UserLoginForm for login and whether the user id or password to login matches the DB.

--- board/shboard/views/auth_views.py
+++ board/shboard/views/auth_views.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash
 from werkzeug.utils import redirect
 
 from shboard import db
-from shboard.forms import UserCreateForm
+from shboard.forms import UserCreateForm, UserLoginForm
 from shboard.models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -24,3 +24,21 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html', form=form)
+
+
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/login.html', form=form)
