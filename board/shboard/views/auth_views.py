c6cfb7a895f803ea4e07367ce3c349d5247d3fc7
kmseunh <tmdgus8779@gmail.com>
2023-05-28 00:26:17 +0900

    feat: Generate a decorator function named login_required in the auth_views.py file
    The login_required function checks for g.user, or redirects to the login URL and executes the original function as it is if g.user is present.
    
    If the request method is GET, communicate the next parameter to the login page so that you can access the page you were originally going to after logging in.

--- board/shboard/views/auth_views.py
+++ board/shboard/views/auth_views.py
@@ -6,6 +6,8 @@ from shboard import db
 from shboard.forms import UserCreateForm, UserLoginForm
 from shboard.models import User
 
+import functools
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
@@ -39,7 +41,11 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user.id
-            return redirect(url_for('main.index'))
+            _next = request.args.get('next', '')
+            if _next:
+                return redirect(_next)
+            else:
+                return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
 
@@ -57,3 +63,14 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('auth.login', next=_next))
+        return view(*args, **kwargs)
+
+    return wrapped_view
