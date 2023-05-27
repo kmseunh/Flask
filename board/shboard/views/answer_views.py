30afd8a8c59aa8ddd5e2e6d90747eb56b76aeceb
kmseunh <tmdgus8779@gmail.com>
2023-05-28 00:19:08 +0900

    feat: Add the ability to save authors when registering answers
    g.user is the user information data stored in the session.

--- board/shboard/views/answer_views.py
+++ board/shboard/views/answer_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template
+from flask import Blueprint, url_for, request, render_template, g
 from werkzeug.utils import redirect
 
 from shboard import db
@@ -12,12 +12,12 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=('POST',))
 def create(question_id):
-    form            = AnswerForm()
-    question        = Question.query.get_or_404(question_id)
+    form = AnswerForm()
+    question = Question.query.get_or_404(question_id)
     if form.validate_on_submit():
-        content     = request.form['content']               #--- POST 폼 방식으로 전송된 데이터 항목 중 name 속성이 content인 값
-        answer      = Answer(content=content, create_date=datetime.now())
-        question.answer_set.append(answer)                  #--- 질문에 달린 답변들
+        content = request.form['content']
+        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
+        question.answer_set.append(answer)
         db.session.commit()
         return redirect(url_for('question.detail', question_id=question_id))
-    return redirect(url_for('question/detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
